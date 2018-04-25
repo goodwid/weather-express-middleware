960358d1cf605286980bdd959f9a77706ca225fe
David Goodwin <goodwid@gmail.com>
2018-04-24 20:06:47 -0700

    Update stuff

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+/* eslint no-console: off */
+const http = require('http');
 const app = require('./lib/app');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT);
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log('server running on', server.address().port);
+});
